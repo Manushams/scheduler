8263c4fb1159a3f9d4a402a3197e298a739bd063
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-09-08 11:06:46 +0500

    sev tasks count + match

--- src/components/table/multipleTasks.js
+++ src/components/table/multipleTasks.js
@@ -17,3 +17,40 @@ const Filter = (tasks, day) => {
     const heighestObj = tasksOnDay.filter(task => task.cellDetails.id === heighest[0])
     return heighestObj
 }
+
+const Count = (task) => {
+    var startHour = parseInt(task.timeStart)
+    var endHour = parseInt(task.timeEnd)
+    var startMinutes = parseInt(task.timeStart.toString().slice(3,5))
+    var endMinutes = parseInt(task.timeEnd.toString().slice(3,5))
+    const startTotalMins = startHour * 60 + startMinutes
+    const endTotalMins = endHour * 60 + endMinutes
+    const totalMins = endTotalMins - startTotalMins
+    const everyMinute = []
+
+    startMinutes = startMinutes - 1
+    for(let i = 0; i <= totalMins; i++){
+        startMinutes++
+        console.log(startMinutes)
+        if(startMinutes === 60){
+            startMinutes = 0;
+            startHour++
+        }
+        everyMinute.push(`${startHour}:${startMinutes}`)
+    }
+    return everyMinute
+}
+
+const Match = (task1, task2) => {
+    const task1Mins = Count(task1);
+    const task2Mins = Count(task2);
+
+    const isMatch = task1Mins.filter(t1 => task2Mins.some(t2 => t1 === t2))
+    console.log(isMatch)
+    if(isMatch.length){
+        return true
+    }else{
+        return false
+    }
+
+}

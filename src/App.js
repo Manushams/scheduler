3b0979f25ae402434b59975c7fe00c1eeb769f7f
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-10-12 23:06:36 +0500

    month table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import Weektable from './components/table/weektable'
 import Weektable2 from './components/table/weektable2'
+import MonthTable from './components/table/monthTable'
 import Navbar from './components/layout/navbar'
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Weektable2} />
           <Route path='/dateTo' component={Weektable} />
+          <Route path='/month' component={MonthTable} />
         </Switch>
       </Router>
       
--- src/components/table/monthTable.js
+++ src/components/table/monthTable.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 class MonthTable extends React.Component{
+
     daysInMonth = (day) => {
-        if(day)return (new Date(day.getYear(), day.getMonth(), 0))
-        if(!day)return (new Date(new Date().getYear(), new Date().getMonth(), 0))
+        if(day)return (new Date(day.getYear(), day.getMonth() + 1, 0).getDate())
+        if(!day)return (new Date(new Date().getYear(), new Date().getMonth() + 1, 0))
     }
+
     render(){
         const td = document.createElement('td')
+        const today = new Date();
+        const dayOfWeek = new Date(today.getFullYear(), today.getMonth(), 1).getDay()
+        
         return(
             <div className='monthtable'>
                 <table>

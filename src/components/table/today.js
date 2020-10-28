b0579ea06da20f7a8a94204e5010931caf1b5fd6
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-10-28 22:39:12 +0500

    today table

--- src/components/table/today.js
+++ src/components/table/today.js
@@ -2,8 +2,43 @@ import React from 'react'
 
 class Today extends React.Component{
 
+    state = {
+        daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        hours: null
+    }
+
+    setHours = () => {
+        const promise = new Promise((resolve, reject) => {
+            let hours = []
+            
+            for(let i = 0; i<24;i++){
+                if(i < 10){
+                    hours.push(`0${i}:00`)
+                }else{
+                    hours.push(`${i}:00`)
+                }
+            }
+
+            if(hours.length === 24){
+                return resolve(hours)
+            }            
+        })
+        let hrs  
+        promise.then((hours) => {
+            hours = hrs
+        })
+        this.setState({
+            hours: hrs
+        })
+    }
+
     render(){
-        const today = new Date()
+        const today = new Date(),
+            {daysOfWeek} = this.state
+        this.setHours()
+        setTimeout(()=> {
+            console.log(this.state)
+        }, 1000)
         return(
             <div className='today'>
                 <div className="top-bar">
@@ -23,11 +58,12 @@ class Today extends React.Component{
                 <table className="table fixed">
                     <thead>
                         <tr className='table-row'>
-                            <th className='table-heading' colSpan='2'>00:00</th>
+                            <th className='table-heading table-heading-today-empty'></th>
+                            <th className='table-heading table-heading-today'>{daysOfWeek[today.getDay()]} {today.getDate()}</th>
                         </tr>
                     </thead>
-                    <tr  className='table-row'><td className='table-data'></td></tr>
-                    <tr  className='table-row'><td className='table-data'></td></tr>
+                    <tr className='table-row'><th>00:00</th><td></td></tr>
+                    <tr className='table-row'><th></th></tr>
                 </table>
             </div>
         )

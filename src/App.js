f84da43958d40175bdf11576b6f8e3d1b43f41b0
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-11-12 21:13:10 +0500

    day component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weektable2 from './components/table/weektable2'
 import MonthTable from './components/table/monthTable'
 import Navbar from './components/layout/navbar'
 import Today from './components/table/today'
+import Day from './components/table/day'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/dateTo' component={Weektable} />
           <Route path='/month' component={MonthTable} />
           <Route path='/today' component={Today} />
+          <Route path='/day' component={Day} />
         </Switch>
       </Router>
       
---
+++ src/components/table/day.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import {connect} from 'react-redux'
+
+class Day extends React.Component{
+    
+    state = {
+        hours: [],
+        day: new Date(),
+    };
+
+    componentDidMount(){
+        this.setHours();
+        this.displayTasks();
+    }
+
+    setHours = () => {
+        
+        const loopHours = new Promise((resolve, reject) => {
+            let hours = []
+            
+            for(let i=0; i<24; i++ ){
+                if(i<10){   
+                    hours.push(`0${i}:00`)
+                }else{
+                    hours.push(`${i}:00`)
+                }
+                
+            }
+            return resolve(hours)
+        } )
+
+        loopHours.then((hours) => {
+            this.setState({
+                hours: [...hours]
+            })
+        })
+
+    }
+    handleDayChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            day: new Date(e.target.value)
+        })
+    }
+
+    displayTasks = () => {
+        const tdParent = document.querySelector('.td-parent'),
+            {tasks} = this.props,
+            div = document.createElement('div')
+        
+        div.classList.add('task-div')
+
+
+
+        tasks.forEach(task => {
+            
+        })
+
+
+    }
+
+
+    render(){
+        setTimeout(() => {
+            console.log(this.state)
+        }, 100);
+        const {day, hours} = this.state,
+            date = day.getDate(),
+            month = day.toLocaleString('default', {month: 'long'}),
+            year = day.getFullYear(),
+            dayWeek = day.toLocaleString('default', {weekday: 'short'})
+
+        return(
+            <div className='day'>
+                <div className="top-bar">
+                    <div>
+                        <h3>
+                            {month} {date}, {year}
+                        </h3>
+                        <input 
+                            id='top-bar-calendar' 
+                            type="date" 
+                            onChange={this.handleDayChange}
+                        />
+                    </div>
+                        <ul>
+                            <li><a href="#!">Today</a></li>
+                            <li><a href="/">Week</a></li>
+                            <li><a href="#!">Work Week</a></li>
+                            <li><a href="/month">Month</a></li>
+                        </ul>
+                </div>
+
+                <table className='table-parent'>
+                    <thead>
+                        <tr>
+                            <td className='td-parent'>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th className='table-heading table-heading-today'></th>
+                                            <th className='table-heading table-heading-today'>{dayWeek}, {date}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {hours.map(hour => {
+                                            return(
+                                                <tr>
+                                                    <th>{hour}</th>
+                                                    <td></td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return{
+        tasks: state.addTask.tasks,
+        modalEnable: state.toggleModal.modalEnable,
+    }
+}
+
+export default Day;

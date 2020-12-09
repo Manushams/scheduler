3ef89f27a144e180010fb0e925c811671fdf4ef3
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-12-09 15:25:52 +0500

    resposnive navbar

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -44,7 +44,10 @@ class Navbar extends React.Component {
             }else{
                 navbar.style.display = 'none';
                 lines.forEach(line => line.style.backgroundColor = '#000');
-                ul.style.display = 'none'            
+                ul.style.display = 'none';
+                lines[1].style.display = 'flex'
+                lines[0].style.transform="rotate(180deg) translate(0,0)"
+                lines[2].style.transform="rotate(180deg)"            
             }
         })
     }
@@ -59,11 +62,13 @@ class Navbar extends React.Component {
             (ul.style.display === 'none')){
                 ul.style.display = 'flex';
                 setTimeout(() => {
+                    dots.style.transform = 'perspective(10rem) translateZ(2rem)';
                     ul.style.opacity = '1';
                     ul.style.transform = 'translateY(.5rem)'                   
                     isShown = true                
                 }, 100);
             }else{
+                dots.style.transform = 'perspective(10rem) translateZ(0rem)';
                 ul.style.display = 'none';
                 ul.style.opacity = '0';
                 ul.style.transform = 'translateY(-3rem)'

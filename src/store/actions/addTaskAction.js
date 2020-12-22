92b2b413bd031deb720761e4b6e7cc8736aefc38
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-12-22 20:49:02 +0500

    adding tasks to firestore

--- src/store/actions/addTaskAction.js
+++ src/store/actions/addTaskAction.js
@@ -1,3 +1,5 @@
+import { getFirebase } from "react-redux-firebase"
+
 export const addTask = (task) => {
     const startHour = parseInt(task.timeStart)
     const endHour = parseInt(task.timeEnd)
@@ -6,9 +8,19 @@ export const addTask = (task) => {
     const startTotalMins = startHour * 60 + startMinutes
     const endTotalMins = endHour * 60 + endMinutes
     const height = (endTotalMins - startTotalMins ) * (2/15)
-
     if(height > 0){
-        return {type: 'ADD_TASK', task}
+        return (dispatch, getState, getFirebase) => {
+            const firestore = getFirebase().firestore();
+            
+            firestore.collection('tasks').doc(task.id.toString()).set({
+                ...task
+            }).then(() =>{
+                console.log(task)
+                dispatch({type: 'ADD_TASK', task})
+            }).catch(err => {
+                dispatch({type: 'ADD_TASK_FAILED', err})
+            })
+        }
     }else{
         return {type: 'ADD_TASK_ERROR', err: 'Wrong Time Input'}
     }
--- src/store/reducers/addTaskReducer.js
+++ src/store/reducers/addTaskReducer.js
@@ -1,39 +1,14 @@
-const initState = {
-    tasks: [
-        {
-            cellDetails: {id: '0.1234512312348', row: 2, title: 'Fri Nov 06 2020'},
-            date: '2020-11-06',
-            eventName: 'hey There',
-            height: 180,
-            id: '0.181213481234',
-            timeEnd: '13:00',
-            timeStart: '10:00'
-        },
-        {
-            cellDetails: {id: '0.123441231234', row: 2, title: 'Fri Nov 06 2020'},
-            date: '2020-11-06',
-            eventName: 'second',
-            height: 31,
-            id: '0.881283481234',
-            timeEnd: '11:42',
-            timeStart: '11:11'
-        },           
-    ],
-    error: null
-}
-
-export const addTask = (state = initState, action)=>{
+export const addTask = (state = [], action)=>{
     switch(action.type){
         case 'ADD_TASK':
-            const newTasks = state.tasks.concat(action.task)
-            console.log('newState',newTasks)
-            return {
-                ...state,
-                tasks: newTasks,
-                error: null
-            }
+            console.log('task successfully added')
+            return state
         
-        case 'ADD_TASK_ERROR':
+        case 'ADD_TASK_FAILED':
+            console.log('error:', action.err)
+            return state;
+
+        case 'ADD_TASK_ERROR':            
             return {
                 ...state,
                 error: action.err 
